eafa1dd31ca652793e398abefb4473ca740fb20a
Azar Safarov <azar.safarov@accenture.com>
2020-09-29 12:15:38 +0300

    React app base created

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Alert } from 'reactstrap'
+
+function App() {
+  return <Alert color="success">This is a success alert — check it out!</Alert>
+}
+
+export default App
